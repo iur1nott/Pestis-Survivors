8a39d8c5f24ae70556add4b7951df821d0c16755
ian <110679442+driize@users.noreply.github.com>
2025-06-10 01:29:16 -0300

    Update inimigo dano variado

--- Assets/Scripts/Inimigos/Inimigos.cs
+++ Assets/Scripts/Inimigos/Inimigos.cs
@@ -2,29 +2,35 @@ using UnityEngine;
 
 public class Todos : MonoBehaviour
 {
-    [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] private Rigidbody2D rigidBody;
-    private Vector3 direção;
+  [SerializeField] private SpriteRenderer spriteRenderer;
+  [SerializeField] private Rigidbody2D rigidBody;
+  private Vector3 direção;
 
-    [SerializeField] private float moveSpeed;
+  [SerializeField] private float moveSpeed;
+  [SerializeField] private float damage;
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    if (PlayerController.Instance.gameObject.activeSelf)
     {
-        if (PlayerController.Instance.gameObject.activeSelf){
-            // se movimenta em direção ao player
-            direção = (PlayerController.Instance.transform.position - transform.position).normalized;
-            rigidBody.linearVelocity = new Vector2(direção.x * moveSpeed, direção.y * moveSpeed);
-        } else {
-            rigidBody.linearVelocity = Vector2.zero;
-        }
+      // se movimenta em direção ao player
+      direção = (PlayerController.Instance.transform.position - transform.position).normalized;
+      rigidBody.linearVelocity = new Vector2(direção.x * moveSpeed, direção.y * moveSpeed);
     }
+    else
+    {
+      rigidBody.linearVelocity = Vector2.zero;
+    }
+  }
 
-    // felipe -- lógica de colisão com player
-    void OnCollisionStay2D(Collision2D collision) {
-        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
-        if (player != null) {
-            player.TakeDamage(1);
-        }
+  // felipe -- lógica de colisão com player
+  void OnCollisionStay2D(Collision2D collision)
+  {
+    if (collision.gameObject.CompareTag("Player"))
+    {
+      PlayerController.Instance.TakeDamage(damage);
+      Destroy(gameObject);
     }
+  }
 }
